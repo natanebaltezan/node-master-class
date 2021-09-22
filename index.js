04be8c82eddf1e64b647feaaa062d9f6c696c20a
natane <natane.baltezan@minutrade.com>
2021-09-22 10:07:49 -0300

    feat:check service

--- index.js
+++ index.js
@@ -79,5 +79,6 @@ httpsServer.listen(config.httpsPort, () => {
 var router = {
   'ping': handlers.ping,
   'users': handlers.users,
-  'tokens': handlers.tokens
+  'tokens': handlers.tokens,
+  'checks': handlers.checks
 };
